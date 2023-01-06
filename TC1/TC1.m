9104ccd8ade4ce26f0c72318c3bc8fbac1f0b033
Andreza Nascimento <andrezacosta110@gmail.com>
2023-01-06 15:47:43 -0300

    Atualização do material

--- TC1/TC1.m
+++ TC1/TC1.m
@@ -10,6 +10,42 @@
 clc;clear;
 
 serie_vazoes = load('furnas.dat');
+
+fig = figure; clf
+
+h = plot(serie_vazoes'); hold on
+
+grid on
+
+% define figure properties
+opts.Colors     = get(groot,'defaultAxesColorOrder');
+opts.saveFolder = 'img/';
+opts.width      = 20;
+opts.height     = 10;
+opts.fontType   = 'Times';
+opts.fontSize   = 24.6;
+
+% add axis labes and legend
+axis tight
+title('Série de Vazões Furnas de Jan/1930 até Dez/1990 com Sazonalidade')
+xlabel('Mês')
+ylabel('Vazão média do reservatório (m³)')
+%legend('1960','1946','1970','1972','1975')
+
+% scaling
+fig.Units               = 'centimeters';
+fig.Position(3)         = opts.width;
+fig.Position(4)         = opts.height;
+
+% set text properties
+set(fig.Children, ...
+    'FontName',     'Times', ...
+    'FontSize',     23);
+
+% remove unnecessary white space
+set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
+hold off
+
 ln_serie = log(serie_vazoes);
 m_serie = mean(ln_serie);
 dp_serie = std(ln_serie);
@@ -164,6 +200,7 @@ clc;clear;
 
 serie_vazoes = load('furnas.dat');
 m_serie = mean(serie_vazoes);
+dp_serie = std(serie_vazoes);
 modulo_serie = reshape((serie_vazoes./m_serie)', 1,[]);
 serie_vazoes = reshape((serie_vazoes)', 1,[]);
 
@@ -221,6 +258,43 @@ axis tight
 title('Série de vazões Furnas de Jan/1930 até Dez/1990 sem Sazonalidade')
 xlabel('Mês')
 ylabel('Vazão média do reservatório (m³)')
+%legend('1960','1946','1970','1972','1975')
+
+% scaling
+fig.Units               = 'centimeters';
+fig.Position(3)         = opts.width;
+fig.Position(4)         = opts.height;
+
+% set text properties
+set(fig.Children, ...
+    'FontName',     'Times', ...
+    'FontSize',     23);
+
+% remove unnecessary white space
+set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
+hold off
+
+%HISTOGRAMA
+
+fig = figure; clf
+
+h = histogram(modulo_serie); hold on
+
+grid on
+
+% define figure properties
+opts.Colors     = get(groot,'defaultAxesColorOrder');
+opts.saveFolder = 'img/';
+opts.width      = 20;
+opts.height     = 10;
+opts.fontType   = 'Times';
+opts.fontSize   = 24.6;
+
+% add axis labes and legend
+axis tight
+title('Histograma da Série de Vazões Furnas sem Sazonalidade')
+%xlabel('Vazão média do reservatório (m³)')
+ylabel('Frequência (Meses)')
 %legend('1960','1946','1970','1972','1975')
 
 % scaling
