70b9841df4380ada02d9a619ec5bd1bcc076feec
Andreza Nascimento <andrezacosta110@gmail.com>
2023-01-06 17:16:41 -0300

    Atualização do script

--- TC1/TC1_draft.m
+++ TC1/TC1_draft.m
@@ -5,51 +5,14 @@
 
 % 1º TRABALHO COMPUTACIONAL
 
-%% Parte 1 - Método Artigo
+%% Parte 1  - Método Alternativo
 
 clc;clear;
 
 serie_vazoes = load('furnas.dat');
-
-fig = figure; clf
-
-h = plot(serie_vazoes'); hold on
-
-grid on
-
-% define figure properties
-opts.Colors     = get(groot,'defaultAxesColorOrder');
-opts.saveFolder = 'img/';
-opts.width      = 20;
-opts.height     = 10;
-opts.fontType   = 'Times';
-opts.fontSize   = 24.6;
-
-% add axis labes and legend
-axis tight
-title('Série de Vazões Furnas de Jan/1930 até Dez/1990 com Sazonalidade')
-xlabel('Mês')
-ylabel('Vazão média do reservatório (m³)')
-%legend('1960','1946','1970','1972','1975')
-
-% scaling
-fig.Units               = 'centimeters';
-fig.Position(3)         = opts.width;
-fig.Position(4)         = opts.height;
-
-% set text properties
-set(fig.Children, ...
-    'FontName',     'Times', ...
-    'FontSize',     23);
-
-% remove unnecessary white space
-set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
-hold off
-
-ln_serie = log(serie_vazoes);
-m_serie = mean(ln_serie);
-dp_serie = std(ln_serie);
-modulo_serie = reshape(((ln_serie-m_serie)./dp_serie)', 1,[]);
+m_serie = mean(serie_vazoes);
+dp_serie = std(serie_vazoes);
+modulo_serie = reshape((serie_vazoes./m_serie)', 1,[]);
 serie_vazoes = reshape((serie_vazoes)', 1,[]);
 
 fig = figure; clf
@@ -68,7 +31,7 @@ opts.fontSize   = 24.6;
 
 % add axis labes and legend
 axis tight
-title('Série de Vazões Furnas de Jan/1930 até Dez/1990 com Sazonalidade')
+title('Série de vazões Furnas de Jan/1930 até Dez/1990 com Sazonalidade')
 xlabel('Mês')
 ylabel('Vazão média do reservatório (m³)')
 %legend('1960','1946','1970','1972','1975')
@@ -103,7 +66,7 @@ opts.fontSize   = 24.6;
 
 % add axis labes and legend
 axis tight
-title('Série de Vazões Furnas de Jan/1930 até Dez/1990 sem Sazonalidade')
+title('Série de vazões Furnas de Jan/1930 até Dez/1990 sem Sazonalidade')
 xlabel('Mês')
 ylabel('Vazão média do reservatório (m³)')
 %legend('1960','1946','1970','1972','1975')
@@ -122,42 +85,7 @@ set(fig.Children, ...
 set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
 hold off
 
-% HISTOGRAMAS
-
-fig = figure; clf
-
-h = histogram(serie_vazoes); hold on
-
-grid on
-
-% define figure properties
-opts.Colors     = get(groot,'defaultAxesColorOrder');
-opts.saveFolder = 'img/';
-opts.width      = 20;
-opts.height     = 10;
-opts.fontType   = 'Times';
-opts.fontSize   = 24.6;
-
-% add axis labes and legend
-axis tight
-title('Histograma da Série de Vazões Furnas com Sazonalidade')
-xlabel('Vazão média do reservatório (m³)')
-ylabel('Frequência (Meses)')
-%legend('1960','1946','1970','1972','1975')
-
-% scaling
-fig.Units               = 'centimeters';
-fig.Position(3)         = opts.width;
-fig.Position(4)         = opts.height;
-
-% set text properties
-set(fig.Children, ...
-    'FontName',     'Times', ...
-    'FontSize',     23);
-
-% remove unnecessary white space
-set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
-hold off
+%HISTOGRAMA
 
 fig = figure; clf
 
@@ -193,5 +121,3 @@ set(fig.Children, ...
 % remove unnecessary white space
 set(gca,'LooseInset',max(get(gca,'TightInset'), 0.02))
 hold off
-
-%% Item 2
